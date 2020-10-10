84ad98b65f9b5c660cf10caf167aa056e1905aa9
John Oram <john@oram.ca>
2020-10-09 20:39:13 -0700

    serve react build

--- api/main.py
+++ api/main.py
@@ -1,17 +1,22 @@
 import atexit
-
-from flask import Flask, request
+import os
+from flask import Flask, request, send_from_directory
 from time import sleep
 from concurrent.futures import ThreadPoolExecutor
 
 done = False
-app = Flask(__name__)
+dir_path = os.path.dirname(os.path.realpath(__file__))
+build_dir = os.path.join(dir_path, "build")
+app = Flask(__name__, static_folder=build_dir)
 state = {}
 
 
-@app.route('/')
-def home():
-    return 'Two jobs were launched in background!'
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def serve(path):
+    if path != "" and os.path.exists(app.static_folder + '/' + path):
+        return send_from_directory(app.static_folder, path)
+    return send_from_directory(app.static_folder, 'index.html')
 
 
 @app.route('/api/v0/state', methods=["GET", "POST"])
@@ -34,7 +39,7 @@ def stop_leds():
 
 
 if __name__ == '__main__':
-    executor = ThreadPoolExecutor(2)
-    executor.submit(drive_leds)
-    atexit.register(stop_leds)
+#    executor = ThreadPoolExecutor(2)
+ #   executor.submit(drive_leds)
+#    atexit.register(stop_leds)
     app.run(host="0.0.0.0", debug=True)
