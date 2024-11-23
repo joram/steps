8a92c36842ad7e2046a9274a739cf39190883930
John Oram <john@oram.ca>
2024-11-23 15:35:00 -0800

    deploy

--- api/main.py
+++ api/main.py
@@ -105,4 +105,4 @@ async def set_mode(body: SetModeRequest):
     return {"success": True}
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", port=5000, log_level="info")
+    uvicorn.run("main:app", port=8000, log_level="info")
